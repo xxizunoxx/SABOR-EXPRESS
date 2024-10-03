74539fd5a6bf7f2150a58b89e374d51ca4f2192b
xxizunoxx <caique_mk2@hotmail.com>
2024-10-03 14:00:19 -0300

    configurações de retorno(prints)

---
+++ sabor-express/app.py
@@ -0,0 +1,11 @@
+print("""
+丂闩⻏ㄖ尺 㠪〤尸尺🝗丂丂
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}!')
---
+++ sabor-express/exercicios.py
@@ -0,0 +1,34 @@
+#1 - Para imprimir a frase, basta utilizar a função print() com aspas simples ou duplas entre a sentença:
+print('Python na Escola de Programação da Alura\n')
+
+#2 - Para imprimir a frase utilizando variáveis, temos algumas abordagens possíveis:
+# Criação das Variáveis
+nome = 'Lais'
+idade = 24
+
+# Abordagem mais simples
+print('Meu nome é',nome,'e tenho',idade,'anos.')
+
+# Abordagem do f-string
+print(f'Meu nome é {nome} e tenho {idade} anos.')
+
+# Abordagem do .format()
+print('Meu nome é {} e tenho {} anos.'.format(nome,idade))
+
+# Abordagem da % (Formatação de String Antiga - Python 2)
+print('Meu nome é %s e tenho %i anos.'%(nome,idade))
+
+#3 - Para imprimir a palavra ‘ALURA’ com cada letra em cada linha com a função print utilizando o parâmetro sep para especificar o separador entre os argumentos da seguinte forma:
+print('A','L','U','R','A',sep='\n')
+
+#4 - Para imprimir o valor de pi arredondado, temos algumas possíveis abordagens:
+pi = 3.14159
+
+# Abordagem de f-string
+print(f'O valor arredondado de pi é: {pi:.2f}')
+
+# Abordagem de .format()
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
+
+# Utilizando a função round()
+print('O valor arredondado de pi é:', round(pi, 2))
