3b52eb3c50ba0c241ccc60a82f70bfabff2ab439
xxizunoxx <caique_mk2@hotmail.com>
2024-10-08 12:16:22 -0300

    novas funções

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Gigante Nordestino', 'Outback', 'Mc Donalds']
+
 #Função para exibir nome do programa.
 def exibir_nome_do_programa():
     print("""
@@ -15,26 +17,56 @@ def exibir_opcoes():
 
 # Função para finalizar o app e limpar o terminal.
 def finalizar_app():
+    exibir_subtitulo('Finalizando o app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal\n')
+    main()
+
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
     os.system('cls')
-    print('Finalizando o app\n')
+    print(texto)
+    print()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+
+def listar_restaurantes():
+    exibir_subtitulo('Lista de restaurantes cadastrados')
+    for x in restaurantes:
+        print(f'.{x}')
+    voltar_ao_menu_principal()
 
 # Função para escolher as opções.
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: ')) # Variável tipada para definir a opção escolhida.
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: ')) # Variável tipada para definir a opção escolhida.
 
-    match opcao_escolhida:
-        case 1:
-            print('Adicionar restaurante\n')
-        case 2:
-            print('Listar restaurantes\n')
-        case 3:
-            print('Ativar restaurante\n')
-        case 4:
-            print('Finalizar app\n')
-        case _:
-            print('Opção inválida!\n')
+        match opcao_escolhida:
+            case 1:
+                cadastrar_novo_restaurante()
+            case 2:
+                listar_restaurantes()
+            case 3:
+                print('Ativar restaurante\n')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
