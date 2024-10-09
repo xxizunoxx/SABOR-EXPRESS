ab64bc675f069d115ba2f3a3f9a396ceadf7c71c
xxizunoxx <caique_mk2@hotmail.com>
2024-10-09 15:50:42 -0300

    classes

---
+++ oo-sabor-express/modelos/restaurante.py
@@ -0,0 +1,11 @@
+class restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = restaurante()
+restaurante_pizza = restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
