569d73dff1baeeba6fc9a2bf4540284561f1ecbb
Caique-Camargo <caique_mk2@hotmail.com>
2024-10-16 19:06:45 -0300

    Add docstring e ajustando receber avaliação

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -1,13 +1,13 @@
 from modelos.restaurante import Restaurante
 
 restaurante_gigante = Restaurante('Gigante Nordestino', 'Nordestina')
-restaurante_gigante.receber_avaliacao('Caique', 9)
-restaurante_gigante.receber_avaliacao('Natalia', 10)
-restaurante_gigante.receber_avaliacao('Caue', 8)
+restaurante_gigante.receber_avaliacao('Caique', 4)
+restaurante_gigante.receber_avaliacao('Natalia', 5)
+restaurante_gigante.receber_avaliacao('Caue', 3.5)
 restaurante_outback = Restaurante('Outback', 'Australiana')
-restaurante_outback.receber_avaliacao('Jonatan', 6)
-restaurante_outback.receber_avaliacao('João', 4)
-restaurante_outback.receber_avaliacao('Rogerio', 9)
+# restaurante_outback.receber_avaliacao('Jonatan', 2)
+# restaurante_outback.receber_avaliacao('João', 4)
+# restaurante_outback.receber_avaliacao('Rogerio', 4.5)
 restaurante_mcdonalds = Restaurante('Mcdonalds', 'Fast Food')
 
 restaurante_outback.alternar_estado()
