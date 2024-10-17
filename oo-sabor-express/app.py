49e877373ea70e057af65a6b315eb4f73edc8620
Caique-Camargo <caique_mk2@hotmail.com>
2024-10-16 21:59:44 -0300

    preparando ambiente

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -5,11 +5,12 @@ restaurante_gigante.receber_avaliacao('Caique', 4)
 restaurante_gigante.receber_avaliacao('Natalia', 5)
 restaurante_gigante.receber_avaliacao('Caue', 3.5)
 restaurante_outback = Restaurante('Outback', 'Australiana')
-# restaurante_outback.receber_avaliacao('Jonatan', 2)
-# restaurante_outback.receber_avaliacao('João', 4)
-# restaurante_outback.receber_avaliacao('Rogerio', 4.5)
+restaurante_outback.receber_avaliacao('Jonatan', 2)
+restaurante_outback.receber_avaliacao('João', 4)
+restaurante_outback.receber_avaliacao('Rogerio', 4.5)
 restaurante_mcdonalds = Restaurante('Mcdonalds', 'Fast Food')
 
+restaurante_gigante.alternar_estado()
 restaurante_outback.alternar_estado()
 
 def main():
